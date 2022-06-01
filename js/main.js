49168871c37b08661c87bcd5e5681380c6eb320d
Lautaro Villar <lautarovillar@outlook.com>
2022-06-01 19:36:50 -0300

    subi archivos por primera vez

---
+++ js/main.js
@@ -0,0 +1,9 @@
+ //ALGORITMO
+ let ingreso = prompt("Ingresa la mercaderia.\n Para salir ingresa FINALIZAR");
+while (ingreso != "FINALIZAR") {
+
+  console.log("La mercaderia ingresada es " + ingreso);
+
+  ingreso = prompt("Ingresa la mercaderia. \n Para salir ingresa FINALIZAR");
+}
+
